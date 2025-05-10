8b6afba1df82fc9f051312ad02a2f1aebd0fc292
Domis382 <naviaeddy@gmail.com>
2025-05-10 11:46:39 -0400

    vercel 9

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ import authRegistroHostRoutes from '../src/routes/registroHost.routes';
 const app = express();
 
 app.use(cors({
-  origin: "http://redibo-back-wtt.vercel.app:3000",
+  origin: "http://redibo-front-wtt.vercel.app/",
   credentials: true,
 }));
 app.use(helmet({
