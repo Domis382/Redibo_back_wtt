ba9816d4423a1f3ca4795090c552ab96db0a5dfc
Domis382 <naviaeddy@gmail.com>
2025-05-10 16:52:21 -0400

    repair1

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -34,12 +34,12 @@ router.get(
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "https://redibo-front-wtt.vercel.app?error=google",
+    failureRedirect: "http://redibo.chickenkiller.com:3000?error=google",
     session: true,
   }),
   (req, res) => {
     // 🔥 Redirige al front para que abra el modal de completar perfil
-    res.redirect("https://redibo-front-wtt.vercel.app/home?googleComplete=true");
+    res.redirect("http://redibo.chickenkiller.com:3000/home?googleComplete=true");
   }
 );
 router.get("/auth/success", (req, res) => {
@@ -71,11 +71,11 @@ router.post("/check-phone", checkPhoneExists);
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "https://redibo-front-wtt.vercel.app/home?error=cuentaExistente",
+    failureRedirect: "http://redibo.chickenkiller.com:3000/home?error=cuentaExistente",
     session: true,
   }),
   (req, res) => {
-    res.redirect("https://redibo-front-wtt.vercel.app/home?googleComplete=true");
+    res.redirect("http://redibo.chickenkiller.com:3000/home?googleComplete=true");
   }
 );
 export default router;
