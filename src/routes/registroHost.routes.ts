ec65c66676300cfc190e684c12143f25bca89a06
Domis382 <naviaeddy@gmail.com>
2025-05-17 01:52:16 -0400

    repairv11

--- src/routes/registroHost.routes.ts
+++ src/routes/registroHost.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import { registrarHostCompletoController } from '@/controllers/authRegistroHost/registroHost.controller';
-import { requireAuth } from '@/middlewares/authMiddleware';
-import upload from '@/middlewares/multer'; // ✅ actualizado con tu multer real
+import { registrarHostCompletoController } from '../controllers/authRegistroHost/registroHost.controller';
+import { requireAuth } from '../middlewares/authMiddleware';
+import upload from '../middlewares/multer'; // ✅ actualizado con tu multer real
 
 const router = express.Router();
 
