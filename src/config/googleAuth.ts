7e93a3ab675a7ddf11ddfa7e6c1615d82acfd7a6
Domis382 <naviaeddy@gmail.com>
2025-06-01 23:10:55 -0400

    3er sprint1

--- src/config/googleAuth.ts
+++ src/config/googleAuth.ts
@@ -12,7 +12,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
       callbackURL:
-      "http://localhost:3001/api/auth/google/callback",
+      "https://redibo-back-wtt.vercel.app/api/auth/google/callback",
   },
   async (_accessToken, _refreshToken, profile, done) => {
     console.log("🔵 Perfil de Google:", profile);
