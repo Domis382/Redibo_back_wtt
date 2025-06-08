fae9da72a6a5fd9419b6f5606b20d60198cf3717
Domis382 <naviaeddy@gmail.com>
2025-06-07 23:20:07 -0400

    a

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,15 @@
-import app from '../src/app';
+import app from "../src/app";
+import ensureDefaultUbicacion from "../src/app";
 import { NowRequest, NowResponse } from '@vercel/node';
 
-export default function handler(req: NowRequest, res: NowResponse) {
-  return app(req, res); // Express maneja la petición directamente
+// Asegura la ejecución de setup antes de la primera request
+let initialized = false;
+
+export default async function handler(req: NowRequest, res: NowResponse) {
+  if (!initialized) {
+    await ensureDefaultUbicacion(req, res);
+    initialized = true;
+  }
+
+  return app(req, res); // Vercel maneja internamente app como handler
 }
