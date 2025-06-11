e02ebf7c155b8c6c83c48990d3bb5087ddea4e43
Domis382 <naviaeddy@gmail.com>
2025-06-10 22:48:44 -0400

    dsadsas

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,5 @@ import app from '../src/app';
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default function handler(req: NowRequest, res: NowResponse) {
-  return app(req, res); // Express maneja la petición directamente
+  return app(req, res); // Express maneja la petición directamente a
 }
